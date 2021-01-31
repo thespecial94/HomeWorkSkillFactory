90b0d283e657c6773af916d789e5f052f5212f71
thespecial94 <45978401+thespecial94@users.noreply.github.com>
2021-01-31 21:37:33 +0300

    Delete Task3.js

--- Task3.js
+++
@@ -1,9 +0,0 @@
-/*Задание 3.
-Написать функцию, которая создает пустой объект, но без прототипа.*/
-
-function getEmptyObject(){
-return newObj = {};
-}
-
-const info = getEmptyObject();
-console.log(info);
